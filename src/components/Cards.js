7af0e4de2c411e43f89c3b963502da39b2ac9a04
RahulGupta77 <rg3498104@gmail.com>
2024-07-22 18:15:56 +0530

    Updated flag name from h4 to h2

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ const Cards = ({ flag }) => {
   return (
     <div className="countryCard">
           <img src={flag.flags.png} alt="flag_image" width={50} height={50} />
-          <h4>{flag.name.common}</h4>
+          <h2>{flag.name.common}</h2>
     </div>
   ); 
 };
